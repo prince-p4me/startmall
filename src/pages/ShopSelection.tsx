2cd33cda2c3ecf5eaa7075f6b9efbaf0766aa158
Prince Kumar <prince.enuke@gmail.com>
2020-05-20 20:46:57 +0530

    trying to implement social login in android

--- src/pages/ShopSelection.tsx
+++ src/pages/ShopSelection.tsx
@@ -48,10 +48,9 @@ const ShopSelection: React.FC = () => {
         // marketList.push(market)
       }
     });
-
   }
 
-  
+
 
   return (
     <IonPage>
@@ -71,7 +70,7 @@ const ShopSelection: React.FC = () => {
             shops={marketList}
           />
         ) : (
-            <p className="no_data_found">We are coming soon in your area.</p> 
+            <p className="no_data_found">We are coming soon in your area.</p>
           )}
       </IonContent>
     </IonPage>
